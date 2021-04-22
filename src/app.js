86bced60d1c9b6d014b91e731b02e99d623733a3
Martin Luaces <martinluaces17@gmail.com>
2021-04-22 14:59:24 +0000

    poker

--- src/app.js
+++ src/app.js
@@ -5,7 +5,52 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateRandomNumber() {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+}
+
+function generateRandomPalo() {
+  let palo = ["heart", "diamond", "club", "spade"];
+  let indexPalo = Math.floor(Math.random() * palo.length);
+
+  return palo[indexPalo];
+}
+
+function limpiarClases() {
+  document.querySelector(".number").classList.remove("heart");
+  document.querySelector(".number").classList.remove("diamond");
+  document.querySelector(".number").classList.remove("club");
+  document.querySelector(".number").classList.remove("spade");
+}
+
+function card() {
+  limpiarClases();
+
+  document.querySelector(".number").classList.add(generateRandomPalo());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  card();
+  let btn = document.getElementById("btnCard");
+
+  btn.addEventListener("click", card);
 };
